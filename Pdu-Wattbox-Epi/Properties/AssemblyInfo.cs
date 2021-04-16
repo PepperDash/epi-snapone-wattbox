4dac9cadd2303fae11e030bcd4adffbab0f6ba89
Trevor Payne <tpayne@pepperdash.com>
2021-04-16 15:17:02 -0500

    Fix Build Actions
    Fix AssemblyInfo

--- Pdu-Wattbox-Epi/Properties/AssemblyInfo.cs
+++ Pdu-Wattbox-Epi/Properties/AssemblyInfo.cs
@@ -4,5 +4,7 @@
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Pdu_Wattbox_Epi")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-buildtype-buildnumber")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildtype-buildnumber")]
 
